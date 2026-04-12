08c298c0282aa7883a46bc156dea24603bed63bc
Vincent <sivilottovincent@gmail.com>
2026-04-13 01:43:41 +0200

    Développement complet de l'app mobile

---
+++ Mobile/ios/FiipWidget/FiipWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  FiipWidgetBundle.swift
+//  FiipWidget
+//
+//  Created by Vincent Sivilotto on 12/04/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FiipWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FiipWidget()
+        FiipWidgetControl()
+    }
+}
